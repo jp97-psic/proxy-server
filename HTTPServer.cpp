d4f675519e8e2c56c67d86b4778d236ef4c3ba6a
ptr97 <ptr.wos@gmail.com>
2019-05-30 22:24:32 +0200

    Refactor

--- HTTPServer.cpp
+++ HTTPServer.cpp
@@ -21,8 +21,9 @@ HTTPServer::HTTPServer() : sockfd(socket(AF_INET, SOCK_STREAM, 0)) {
 HTTPServer::~HTTPServer() {
 	close(sockfd);
 
-  for(auto& fd: fds)
+  for(auto& fd: fds) {
     close(fd.fd);
+  }
 }
 
 void HTTPServer::makeNonBlocking() {
@@ -37,10 +38,12 @@ void HTTPServer::listenOnAddress(int port, const char* ip) {
   sin.sin_port = htons(port);
   sin.sin_addr.s_addr = inet_addr(ip);  
   unsigned int size = sizeof(sin);
+
   if(bind(sockfd, (sockaddr*) &sin, size) == -1) {
     perror("bind");
     exit(1);
   }
+
   if(listen(sockfd, 1) == -1) {
     perror("listen");
     exit(1);
@@ -48,7 +51,7 @@ void HTTPServer::listenOnAddress(int port, const char* ip) {
 }
 
 void HTTPServer::serve() {
-  fds.push_back({sockfd, POLLIN, 0});
+  fds.push_back({ sockfd, POLLIN, 0 });
 
   while(true)
     handleEvents();
@@ -62,12 +65,13 @@ void HTTPServer::handleEvents() {
 
   for(unsigned i = 0; i < fds.size(); ++i)
     if(fds[i].revents & POLLIN) {
-      if(fds[i].fd == sockfd)
+      if(fds[i].fd == sockfd) {
         startNewConnection();
-      else {
+      } else {
         currentFdIndex = i;
-        if(receiveMessage())
+        if(receiveMessage()) {
           reactToMessage();
+        }
       }
     }
 }
@@ -78,10 +82,11 @@ void HTTPServer::startNewConnection() {
   unsigned int client_len = sizeof(client);
 
   int receiver = accept(sockfd, (sockaddr*) &client, &client_len);
-  if(receiver == -1)
+  if(receiver == -1) {
     perror("accept");
-  else
-    fds.push_back({receiver, POLLIN | POLLOUT, 0});
+  } else {
+    fds.push_back({ receiver, POLLIN | POLLOUT, 0 });
+  }
 }
 
 bool HTTPServer::receiveMessage() {
@@ -94,6 +99,16 @@ bool HTTPServer::receiveMessage() {
   }
 
   if(received == 0) {
+    closeConnection();
+    return false;
+  }
+
+  if(received > 8000) {
+    std::cout << "[ERROR] 413 Payload Too Large" << std::endl;
+    std::string answer = "HTTP/1.0 413 Payload Too Large \r\n\r\n";
+    if(send(fds[currentFdIndex].fd, answer.data(), answer.length(), MSG_NOSIGNAL) == -1) {
+      perror("send");
+    }
     closeConnection();
     return false;
   }
@@ -116,21 +131,20 @@ void HTTPServer::reactToMessage() {
 
   if(endOfRequest()) {
     printInfo();
-    if(method == "CONNECT");
-
-    else
+    if(method == "CONNECT") {
+      std::cout << "Method CONNECT" < std::endl;
+    } else {
       sendResponse();
-
+    }
   }
 }
 
 void HTTPServer::endIfNotHTTPRequest() {
   if(buffer.find("HTTP/") == std::string::npos) {
-    std::string answer = "501 Not Implemented";
-    if(send(fds[currentFdIndex].fd, answer.data(), answer.length(), MSG_NOSIGNAL) == -1)
+    std::string answer = "HTTP/1.0  501 Not Implemented \r\n\r\n";
+    if(send(fds[currentFdIndex].fd, answer.data(), answer.length(), MSG_NOSIGNAL) == -1) {
       perror("send");
-    // std::cout << "// sent: " << filePath << std::endl << std::endl;
-
+    }
     closeConnection();
     query = "";  
   }
@@ -142,43 +156,47 @@ void HTTPServer::setMethodInfo() {
 
 void HTTPServer::setContentInfo() {
   if(contentLength == 0) { // contentLength variable not set yet
-    std::string lengthStr = query.substr(query.find("Content-Length")+16);
+    std::string lengthStr = query.substr(query.find("Content-Length") + 16);
     lengthStr = lengthStr.substr(0, lengthStr.find("\r\n"));
     contentLength = std::atoi(lengthStr.c_str());
+    std::cout << "Received Content-Length = " << contentLength << std::endl;
   }
-  int contentReceived = query.length()-query.find("\r\n\r\n")-4;
-  contentLeft = contentLength-contentReceived;
+
+  int contentReceived = query.length() - query.find("\r\n\r\n") - 4;
+  contentLeft = contentLength - contentReceived;
 }
 
 void HTTPServer::printInfo() {
-  std::cout << query.substr(0, query.find("\r\n\r\n")) << std::endl;
+  std::cout << "REQUEST HEADER: \n" << query.substr(0, query.find("\r\n\r\n")) << std::endl;
   if(method == "POST") {
-    std::string content = query.substr(query.find("\r\n\r\n")+4);
-    std::cout << "// received: " << content << std::endl;
+    std::string content = query.substr(query.find("\r\n\r\n") + 4);
+    std::cout << "REQUEST BODY: \n" << content << std::endl;
   }
 }
 
 void HTTPServer::sendResponse() {
   std::string hostname = getHostname();
   std::string filePath = getFilePath(hostname);
-  std::string answer = getAnswer(hostname,filePath);
-  if(send(fds[currentFdIndex].fd, answer.data(), answer.length(), MSG_NOSIGNAL) == -1)
+  std::string answer = getAnswer(hostname, filePath);
+  if(send(fds[currentFdIndex].fd, answer.data(), answer.length(), MSG_NOSIGNAL) == -1) {
     perror("send");
-  std::cout << "// sent: " << filePath << std::endl << std::endl;
+  }
+    
+  std::cout << "RESPONSE BODY: \n" << filePath << std::endl << std::endl;
 
   closeConnection();
   query = "";  
 }
 
 std::string HTTPServer::getHostname() {
-  std::string hostname = query.substr(query.find("Host")+4+2);
+  std::string hostname = query.substr(query.find("Host") + 4 + 2);
   hostname = hostname.substr(0, hostname.find("\r\n"));
   return hostname;
 }
 
 std::string HTTPServer::getFilePath(std::string hostname) {
   int begin = query.find(hostname) + hostname.length();
-  int length = query.find("HTTP/") - begin-1;
+  int length = query.find("HTTP/") - begin - 1;
   std::string filePath = query.substr(begin, length);
   return filePath;
 }
@@ -198,8 +216,7 @@ std::string HTTPServer::getAnswer(std::string hostname, std::string filePath) {
   fcntl(serverSocket, F_SETFL, flags);
 
   hostent *host = gethostbyname(hostname.data());
-  if(host == NULL)
-  {
+  if(host == NULL) {
     printf("%s is unavailable\n", hostname.data());
     exit(1);
   }
@@ -207,9 +224,9 @@ std::string HTTPServer::getAnswer(std::string hostname, std::string filePath) {
   sockaddr_in sin;
   memset(&sin, 0, sizeof(sin));
   sin.sin_family = AF_INET;
+  // TODO: add https 443
   sin.sin_port = htons(80);
   sin.sin_addr.s_addr = inet_addr(inet_ntoa(*(in_addr*) (host -> h_addr_list[0])));
-
 
   if(connect(serverSocket, (sockaddr*) &sin, sizeof(sin)) == -1 && errno != 115) {
     perror("connect");
@@ -217,10 +234,10 @@ std::string HTTPServer::getAnswer(std::string hostname, std::string filePath) {
   }
 
   std::vector<pollfd> innerFds;
-  innerFds.push_back({serverSocket, POLLOUT | POLLIN});
+  innerFds.push_back({ serverSocket, POLLOUT | POLLIN });
 
   std::string newQuery = method + " " + filePath + query.substr(query.find(" HTTP/"));
-  std::cout << newQuery;
+  std::cout << "New query: " << newQuery << std::endl;
 
   int sent = 0;
   while(sent != newQuery.length()) {
@@ -228,10 +245,11 @@ std::string HTTPServer::getAnswer(std::string hostname, std::string filePath) {
 
     if(innerFds[0].revents & POLLOUT) {
       int status = send(serverSocket, newQuery.data()+sent, newQuery.length()-sent, 0);
-      if(status == -1)
+      if(status == -1) {
         perror("send");
-      else
+      } else {
         sent += status;
+      }
     }
   }
   
@@ -245,7 +263,7 @@ std::string HTTPServer::getAnswer(std::string hostname, std::string filePath) {
       received.resize(status);
     }
   }
-  std::cout << "\r\n// Received: \r\n" << received << "\n";
+  std::cout << "\nRESPONSE received as client: \n" << received << "\n";
   return received;
 }
 
@@ -253,56 +271,10 @@ void HTTPServer::connectToServer(std::string hostname) {
 
 }
 
-std::string HTTPServer::formAnswer(std::string filePath) {
-  std::string answer = "HTTP/1.0 ";
-
-  try {
-    std::vector<char> file = readFile(filePath.c_str());
-    answer += "200 OK\r\n";
-    answer += "Content-Type: ";
-
-    std::string type = filePath.substr(filePath.find(".")+1);
-    if(type == "html")
-      answer += "text/html; charset=UTF-8";
-    else if(type == "jpg")
-      answer += "image/jpeg";
-    else if(type == "js")
-      answer += "application/javascript";
-    else
-      answer += "application/octet-stream";
-
-    answer += "\r\n";
-    answer += "Content-Length: " + std::to_string(file.size()) + "\r\n\r\n";
-    for(char l: file)
-      answer += l;
-    answer += "\r\n\r\n";
-  }
-  catch(std::ios_base::failure) {
-    answer += "404\r\n\r\n";
-  }
-
-  return answer;
-}
-
 void HTTPServer::closeConnection() {
   close(fds[currentFdIndex].fd);
-  fds.erase(fds.begin()+currentFdIndex);
+  fds.erase(fds.begin() + currentFdIndex);
   query = "";
   contentLength = 0;
   contentLeft = 100;
-}
-
-// function throws an exception ios_base::failure if file does not exist (or something goes wrong)
-std::vector<char> HTTPServer::readFile (const char* path) {
-  using namespace std;
-
-  vector<char> result;
-  ifstream file (path, ios::in|ios::binary|ios::ate);
-  file.exceptions ( std::ifstream::failbit | std::ifstream::badbit );
-  result.resize(file.tellg(), 0);
-  file.seekg (0, ios::beg);
-  file.read (result.data(), result.size());
-  file.close();
-
-  return result;
 }
